c1e039ec4ff1811e890182decf38b17d223ae3b6
Ahmed Alshatebi <ahmedalshatibi22@gmail.com>
2025-08-06 16:47:08 +0300

    update page.tsx

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,23 +1,9 @@
 'use client';
 
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
 
 export function ThemeToggle() {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return (
-      <button className="p-2 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800">
-        <div className="w-5 h-5" />
-      </button>
-    );
-  }
 
   return (
     <button
